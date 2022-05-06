36b7036c6d79b03f99a9e85dec6ec3bcfc0430ef
Dobrovolskyi Anatolii <tolik@DESKTOP-911H15M.(none)>
2022-05-06 23:35:21 +0300

    type count accounted

--- src/GameManager.js
+++ src/GameManager.js
@@ -11,10 +11,20 @@ export function gameManager(gameWorld) {
         collisionAnimations,
     } = gameWorld;
 
-    let [plateOnScreenCount, plateTemplate] = getPlateTemplate(stats);
-    for (let i = 0; i < (plateOnScreenCount - plates.length); i++) {
-        CreateNewPlate(plateTemplate, plates)
-    }
+    let plateConfig = getPlateTemplate(stats);
+    // let {count, type} = plateConfig[0];
+    plateConfig.forEach(({type, count}) => {
+        let presentSpritesCount = 0;
+        plates.forEach(({sprite}) => {
+            if (sprite.includes(type)) {
+                presentSpritesCount++
+            }
+        })
+        
+        for (let i = 0; i < (count - presentSpritesCount); i++) {
+            CreateNewPlate(plateTypes[type], plates)
+        }
+    })
 }
 
 // plateTemplate -> [ ] -> newPlate
@@ -34,40 +44,81 @@ function CreateNewPlate(plateTemplate, plates) {
 }
 
 function getPlateTemplate(stats) {
-    let plateTemplate = {
-        speed: 0,
-        horizontalSpeed: 0,
-        sprite: '',
-    }
-    let plateOnScreenCount = 1;
+    let plateConfig = []
     if (stats.score >=40) {
-        plateOnScreenCount = 1;
-        plateTemplate.speed = 12
-        plateTemplate.horizontalSpeed = 0.35
-        plateTemplate.sprite = './speed4.png'
+        plateConfig = [
+            {
+                count: 1,
+                type: 'speed4',
+            }
+        ]
     }
     else if (stats.score >=20) {
-        plateOnScreenCount = 1;
-        plateTemplate.speed = 8
-        plateTemplate.horizontalSpeed = 2
-        plateTemplate.sprite = './speed3.png'
+        plateConfig = [
+            {
+                count: 1,
+                type: 'speed3',
+            }
+        ]
+    }
+    else if (stats.score >=15) {
+        plateConfig = [
+            {
+                count: 1,
+                type: 'speed2',
+            },
+            {
+                count: 1,
+                type: 'speed1',
+            }
+        ]
     }
     else if (stats.score >=10) {
-        plateOnScreenCount = 1;
-        plateTemplate.speed = 6
-        plateTemplate.horizontalSpeed = 3
-        plateTemplate.sprite = './speed2.png'
+        plateConfig = [
+            {
+                count: 1,
+                type: 'speed2',
+            }
+        ]
     }
     else if (stats.score >=5) {
-        plateOnScreenCount = 2;
-        plateTemplate.speed = 3
-        plateTemplate.horizontalSpeed = 7
-        plateTemplate.sprite = './speed1.png'
+        plateConfig = [
+            {
+                type: 'speed1',
+                count: 2,
+            }
+        ]
     }
     else if (stats.score >=0) {
-        plateTemplate.speed = 3
-        plateTemplate.horizontalSpeed = 7
-        plateTemplate.sprite = './speed1.png'
+        plateConfig = [
+            {
+                type: 'speed1',
+                count: 1,
+            },
+        ]
     }
-    return [plateOnScreenCount, plateTemplate]
+    return plateConfig
+}
+
+const plateTypes = {
+    'speed1': {
+        speed: 3,
+        horizontalSpeed: 7,
+        sprite: './speed1.png',
+    },
+    'speed2': {
+        speed: 6,
+        horizontalSpeed: 3,
+        sprite: './speed2.png',
+    },
+    'speed3': {
+        speed: 8,
+        horizontalSpeed: 2,
+        sprite: './speed3.png',
+    },
+    'speed4': {
+        speed: 12,
+        horizontalSpeed: 0.35,
+        sprite: './speed4.png',
+    }
 }
